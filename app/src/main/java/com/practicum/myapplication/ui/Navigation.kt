6e444e4da076cc85b5ca6e706c0fbe8cac882c82
KhizrievMagomed <166821045+smokerm7@users.noreply.github.com>
2026-06-03 23:23:47 +0300

    Update Navigation.kt

--- app/src/main/java/com/practicum/myapplication/ui/Navigation.kt
+++ app/src/main/java/com/practicum/myapplication/ui/Navigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -11,26 +12,42 @@ import com.practicum.myapplication.ui.search.SearchScreen
 import com.practicum.myapplication.ui.search.SearchViewModel
 import com.practicum.myapplication.ui.settings.SettingsScreen
 
-enum class PlaylistScreen(val route: String) {
-    Main("main"),
-    Search("search"),
-    Settings("settings")
+sealed class PlaylistScreen(
+    val route: String
+) {
+    data object Main : PlaylistScreen("main")
+    data object Search : PlaylistScreen("search")
+    data object Settings : PlaylistScreen("settings")
 }
 
 @Composable
 fun PlaylistHost() {
     val navController = rememberNavController()
 
+    PlaylistNavHost(
+        navController = navController
+    )
+}
+
+@Composable
+private fun PlaylistNavHost(
+    navController: NavHostController
+) {
     NavHost(
         navController = navController,
         startDestination = PlaylistScreen.Main.route
     ) {
         composable(PlaylistScreen.Main.route) {
             MainScreen(
-                onNavigateToSearch = { navController.navigate(PlaylistScreen.Search.route) },
-                onNavigateToSettings = { navController.navigate(PlaylistScreen.Settings.route) }
+                onNavigateToSearch = {
+                    navController.navigate(PlaylistScreen.Search.route)
+                },
+                onNavigateToSettings = {
+                    navController.navigate(PlaylistScreen.Settings.route)
+                }
             )
         }
+
         composable(PlaylistScreen.Search.route) {
             val searchViewModel: SearchViewModel = viewModel(
                 factory = SearchViewModel.getViewModelFactory()
@@ -39,11 +56,18 @@ fun PlaylistHost() {
             SearchScreen(
                 modifier = Modifier.fillMaxSize(),
                 viewModel = searchViewModel,
-                onNavigateBack = { navController.popBackStack() }
+                onNavigateBack = {
+                    navController.popBackStack()
+                }
             )
         }
+
         composable(PlaylistScreen.Settings.route) {
-            SettingsScreen(onNavigateBack = { navController.popBackStack() })
+            SettingsScreen(
+                onNavigateBack = {
+                    navController.popBackStack()
+                }
+            )
         }
     }
 }
