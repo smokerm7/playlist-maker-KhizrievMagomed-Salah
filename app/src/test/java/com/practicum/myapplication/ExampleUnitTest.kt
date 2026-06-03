ebc6d3c41f74ea1cd72d93c1409ed2cc66a2d565
KhizrievMagomed <166821045+smokerm7@users.noreply.github.com>
2026-06-03 23:44:43 +0300

    Update ExampleUnitTest.kt

--- app/src/test/java/com/practicum/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/practicum/myapplication/ExampleUnitTest.kt
@@ -1,17 +1,17 @@
 package com.practicum.myapplication
 
+import com.practicum.myapplication.creator.Storage
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
-import org.junit.Assert.*
+class StorageTest {
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun search_returns_tracks() {
+        val storage = Storage()
+
+        val result = storage.search("Miyagi")
+
+        assertTrue(result.isNotEmpty())
     }
 }
