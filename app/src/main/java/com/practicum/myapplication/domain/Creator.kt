2098d3c05c21c49c626f1b0cd80fe2d1bca2a775
KhizrievMagomed <166821045+smokerm7@users.noreply.github.com>
2026-06-03 23:00:02 +0300

    Update Creator.kt

--- app/src/main/java/com/practicum/myapplication/domain/Creator.kt
+++ app/src/main/java/com/practicum/myapplication/domain/Creator.kt
@@ -1,11 +1,15 @@
 package com.practicum.myapplication.domain
 
 import com.practicum.myapplication.creator.Storage
-import com.practicum.myapplication.data.network.RetrofitNetworkClient
+import com.practicum.myapplication.data.network.LocalNetworkClient
 import com.practicum.myapplication.data.network.TracksRepositoryImpl
 
 object Creator {
+
     fun getTracksRepository(): TracksRepository {
-        return TracksRepositoryImpl(RetrofitNetworkClient(Storage()))
+        val storage = Storage()
+        val networkClient = LocalNetworkClient(storage)
+
+        return TracksRepositoryImpl(networkClient)
     }
 }
