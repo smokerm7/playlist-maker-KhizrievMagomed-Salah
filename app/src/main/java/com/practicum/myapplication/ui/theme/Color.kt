a21df26bbb5d669cf841f308c9d0d877b4793610
KhizrievMagomed <166821045+smokerm7@users.noreply.github.com>
2026-06-03 23:17:23 +0300

    Update Color.kt

--- app/src/main/java/com/practicum/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/practicum/myapplication/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package com.practicum.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Blue80 = Color(0xFFBFDBFE)
+val BlueGrey80 = Color(0xFFD6E4F0)
+val Sky80 = Color(0xFFE0F2FE)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Blue40 = Color(0xFF2563EB)
+val BlueGrey40 = Color(0xFF475569)
+val Sky40 = Color(0xFF0284C7)
+
+val PrimaryText = Color(0xFF111827)
+val SecondaryText = Color(0xFF6B7280)
+val IconTint = Color(0xFF9CA3AF)
+
+val SearchBackground = Color(0xFFF3F4F6)
+val DividerColor = Color(0xFFE5E7EB)
