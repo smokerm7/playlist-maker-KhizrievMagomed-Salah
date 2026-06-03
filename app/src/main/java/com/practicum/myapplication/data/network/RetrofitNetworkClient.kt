7f10ec1151127372868813d4e1fe81c6e98b4242
KhizrievMagomed <166821045+smokerm7@users.noreply.github.com>
2026-06-03 22:56:43 +0300

    Update RetrofitNetworkClient.kt

--- app/src/main/java/com/practicum/myapplication/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/com/practicum/myapplication/data/network/RetrofitNetworkClient.kt
@@ -5,10 +5,22 @@ import com.practicum.myapplication.data.dto.TracksSearchRequest
 import com.practicum.myapplication.data.dto.TracksSearchResponse
 import com.practicum.myapplication.domain.NetworkClient
 
-class RetrofitNetworkClient(private val storage: Storage) : NetworkClient {
+class RetrofitNetworkClient(
+    private val storage: Storage
+) : NetworkClient {
 
     override fun doRequest(request: Any): TracksSearchResponse {
-        val searchList = storage.search((request as TracksSearchRequest).expression)
-        return TracksSearchResponse(searchList).apply { resultCode = 200 }
+
+        if (request !is TracksSearchRequest) {
+            return TracksSearchResponse(emptyList()).apply {
+                resultCode = 400
+            }
+        }
+
+        val tracks = storage.search(request.expression)
+
+        return TracksSearchResponse(tracks).apply {
+            resultCode = 200
+        }
     }
 }
